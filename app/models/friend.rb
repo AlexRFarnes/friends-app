2adf503bc9f96e49040093b861869ec1227fce69
Alex <alexrfarnes@gmail.com>
2022-06-02 17:49:24 +0800

    Revert "1"
    This reverts commit 7f77d423a42a04f69ae074d260c723ea37907b42.

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -10,12 +10,6 @@
 #  twitter    :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :integer
-#
-# Indexes
-#
-#  index_friends_on_user_id  (user_id)
 #
 class Friend < ApplicationRecord
-    belongs_to :user
 end
