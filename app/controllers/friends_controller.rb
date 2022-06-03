2999dd0c71ea7c65f5bb30e550966753756cab6c
Alex <alexrfarnes@gmail.com>
2022-06-03 19:49:31 +0800

    Format friends table

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -31,8 +31,9 @@ class FriendsController < ApplicationController
 
     respond_to do |format|
       if @friend.save
-        format.html { redirect_to friend_url(@friend), notice: "Friend was successfully created." }
-        format.json { render :show, status: :created, location: @friend }
+        # friend_url(@friend)
+        format.html { redirect_to friends_path, notice: "Friend was successfully created." }
+        format.json { render :index, status: :created, location: @friend }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @friend.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index'
+  # home#index works for unsigned users
   root 'home#index'
   get 'home/about'
+  # Redirect to friends list after sign up or sign in
+  get '/user' => 'friends#index', :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
